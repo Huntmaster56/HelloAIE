b1cd53c7a5d7273713e224fd462b25a7ed7a6aeb
huntmaster56 <joshwl98@comcast.net>
2016-08-22 12:23:24 -0700

    added some more text

---
+++ Hello AIE/ElectronicLovePotato.cpp
@@ -0,0 +1,13 @@
+//
+// Name: Josh Livingston <josh.livingston@students.aie.edu.au>
+//
+
+#include <iostream>
+
+int main()
+{
+	printf("Hello AIE. My name is Josh Livingston.");
+	printf("This is some more text.");
+
+	getchar();
+}
